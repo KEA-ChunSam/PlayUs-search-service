f8ce76961dd0f099261ebcb5cdb285ee79d98dee
han16935 <han1693516@gmail.com>
2025-05-28 20:39:37 +0900

    refactor : search() service 추상화

--- src/main/java/com/playus/searchservice/domain/post/service/PostSearchService.java
+++ src/main/java/com/playus/searchservice/domain/post/service/PostSearchService.java
@@ -3,16 +3,18 @@ package com.playus.searchservice.domain.post.service;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
 import com.playus.searchservice.domain.common.adapter.TrendingKeywordAdapter;
 import com.playus.searchservice.domain.common.feign.client.UserFeignClient;
+import com.playus.searchservice.domain.common.feign.response.PartyWriterInfoFeignResponse;
 import com.playus.searchservice.domain.post.document.PostDocument;
 import com.playus.searchservice.domain.post.dto.search.SearchRequest;
 import com.playus.searchservice.domain.post.dto.search.SearchResponse;
 import com.playus.searchservice.domain.post.dto.trending.TrendingKeywordResponse;
+import com.playus.searchservice.domain.post.vo.PostSearchResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,30 +36,23 @@ public class PostSearchService {
 
     public SearchResponse search(SearchRequest request) {
 
+        String query = request.query();
+
         Query titleMatchQuery = MatchQuery.of(m -> m
                         .query(request.query())
                         .field("title")
                         .fuzziness("AUTO"))
                 ._toQuery();
 
-//        List<Query> filters = new ArrayList<>();
-//
-//        Query notDeletedPostFilter = TermQuery.of(t -> t
-//                .field("activated")
-//                .value(true)
-//        )._toQuery();
-//        filters.add(notDeletedPostFilter);
-//
-//        Query notSecretPostFilter = TermQuery.of(t -> t
-//                .field("is_secret")
-//                .value(false)
-//        )._toQuery();
-//        filters.add(notSecretPostFilter);
+        List<Query> filters = new ArrayList<>();
+
+        createSoftAndSecretFilter(filters);
 
         Query boolQuery = BoolQuery.of(b -> b
-                        .must(titleMatchQuery))
-//                        .filter(filters))
+                        .must(titleMatchQuery)
+                        .filter(filters))
                 ._toQuery();
+
 
         NativeQuery nativeQuery = NativeQuery.builder()
                 .withQuery(boolQuery)
@@ -64,34 +60,15 @@ public class PostSearchService {
 
         SearchHits<PostDocument> searchResult = elasticsearchOperations.search(nativeQuery, PostDocument.class);
 
-//        List<PostSearchResult> resultList = searchResult.getSearchHits().stream()
-//                .map(SearchHit::getContent)
-//                .toList();
+        increaeTrendingKeyword(query);
 
-        List<SearchHit<PostDocument>> searchHits = searchResult.getSearchHits();
-        System.out.println(searchHits);
+        List<PostSearchResult> resultList = searchResult.getSearchHits().stream()
+                .map(result -> result.getContent().toPostSearchResult())
+                .toList();
 
-//        List<Long> writerIdList = resultList.stream()
-//                .map(PostSearchResult::getWriterId)
-//                .toList();
-//
-//        List<PartyWriterInfoFeignResponse> writerInfoList = userFeignClient.getWriterInfo(writerIdList);
-//
-//// writerId → writerInfo 맵으로 변환
-//        Map<Long, PartyWriterInfoFeignResponse> writerInfoMap = writerInfoList.stream()
-//                .collect(Collectors.toMap(PartyWriterInfoFeignResponse::id, Function.identity(), (a, b) -> a)); // 중복 ID 있을 경우 첫 번째 유지
-//
-//// 각 PostSearchResult 에 writerName 설정
-//        for (PostSearchResult post : resultList) {
-//            PartyWriterInfoFeignResponse writerInfo = writerInfoMap.get(post.getWriterId());
-//            if (writerInfo != null) {
-//                post.updateWriterName(writerInfo.writerName());
-//            }
-//        }
-//
-//        return SearchResponse.of(resultList.size(), resultList);
+        updateWriterInfo(resultList);
 
-        return null;
+        return SearchResponse.of(resultList.size(), resultList);
     }
 
     public List<TrendingKeywordResponse> getTrendingKeyword() {
@@ -108,5 +85,45 @@ public class PostSearchService {
                 .filter(Objects::nonNull)
                 .map(keyword -> TrendingKeywordResponse.of(rank.getAndIncrement(), keyword))
                 .toList();
+    }
+
+    private void updateWriterInfo(List<PostSearchResult> resultList) {
+        List<Long> writerIdList = resultList.stream()
+                .map(PostSearchResult::getWriterId)
+                .toList();
+
+        List<PartyWriterInfoFeignResponse> writerInfoList = userFeignClient.getWriterInfo(writerIdList);
+
+        Map<Long, PartyWriterInfoFeignResponse> writerInfoMap = writerInfoList.stream()
+                .collect(Collectors.toMap(PartyWriterInfoFeignResponse::id, Function.identity(), (a, b) -> a)); // 중복 ID 있을 경우 첫 번째 유지
+
+        for (PostSearchResult post : resultList) {
+            PartyWriterInfoFeignResponse writerInfo = writerInfoMap.get(post.getWriterId());
+            if (writerInfo != null) {
+                post.updateWriterName(writerInfo.writerName());
+            }
+        }
+    }
+
+    private static void createSoftAndSecretFilter(List<Query> filters) {
+        Query notDeletedPostFilter = TermQuery.of(t -> t
+                .field("activated")
+                .value(true)
+        )._toQuery();
+        filters.add(notDeletedPostFilter);
+
+        Query notSecretPostFilter = TermQuery.of(t -> t
+                .field("is_secret")
+                .value(false)
+        )._toQuery();
+        filters.add(notSecretPostFilter);
+    }
+
+    private void increaeTrendingKeyword(String query) {
+        if (trendingKeywordAdapter.existsKeyword(query)) {
+            trendingKeywordAdapter.scoreKeyword(query);
+        } else {
+            trendingKeywordAdapter.addKeyword(query);
+        }
     }
 }
