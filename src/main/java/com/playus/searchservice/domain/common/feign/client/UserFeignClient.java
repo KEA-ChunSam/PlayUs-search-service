d24019a685e25c5362f42d6249bd65cf495692e6
han16935 <han1693516@gmail.com>
2025-06-01 16:10:12 +0900

    refactor : 토큰 블랙리스트 체크 위한 FeignClient 작성

--- src/main/java/com/playus/searchservice/domain/common/feign/client/UserFeignClient.java
+++ src/main/java/com/playus/searchservice/domain/common/feign/client/UserFeignClient.java
@@ -1,9 +1,12 @@
 package com.playus.searchservice.domain.common.feign.client;
 
 import com.playus.searchservice.domain.common.feign.fallback.UserFeignFallback;
+import com.playus.searchservice.domain.common.feign.request.TokenValidationRequest;
 import com.playus.searchservice.domain.common.feign.response.PartyWriterInfoFeignResponse;
+import com.playus.searchservice.domain.common.feign.response.TokenValidationResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,4 +18,7 @@ public interface UserFeignClient {
 
     @PostMapping("/writers")
     List<PartyWriterInfoFeignResponse> getWriterInfo(@RequestBody List<Long> writerIdList);
+
+    @PostMapping("/token/blacklist-check")
+    ResponseEntity<TokenValidationResponse> checkBlackList(@RequestBody TokenValidationRequest req);
 }
--- src/main/java/com/playus/searchservice/domain/common/feign/fallback/UserFeignFallback.java
+++ src/main/java/com/playus/searchservice/domain/common/feign/fallback/UserFeignFallback.java
@@ -1,8 +1,11 @@
 package com.playus.searchservice.domain.common.feign.fallback;
 
 import com.playus.searchservice.domain.common.feign.client.UserFeignClient;
+import com.playus.searchservice.domain.common.feign.request.TokenValidationRequest;
 import com.playus.searchservice.domain.common.feign.response.PartyWriterInfoFeignResponse;
+import com.playus.searchservice.domain.common.feign.response.TokenValidationResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,5 +18,11 @@ public class UserFeignFallback implements UserFeignClient {
     public List<PartyWriterInfoFeignResponse> getWriterInfo(List<Long> writerIdList) {
         log.error("503 happened in UserFeignClient at fetching writer data!!!");
         return List.of(PartyWriterInfoFeignResponse.withServiceUnavailable());
+    }
+
+    @Override
+    public ResponseEntity<TokenValidationResponse> checkBlackList(TokenValidationRequest req) {
+        log.error("503 happened in UserFeignClient at validating token!!!");
+        return ResponseEntity.internalServerError().body(TokenValidationResponse.of(true));
     }
 }
